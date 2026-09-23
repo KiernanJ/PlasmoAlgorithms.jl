4cf44c867e72e96de2f9d51cdac753200e02ffef
Kiernan Jennings <kiernan.xj@gmail.com>
2026-09-23 16:03:03 -0500

    Update solution.jl

--- lib/PlasmoBenders/src/solution.jl
+++ lib/PlasmoBenders/src/solution.jl
@@ -374,6 +374,7 @@ function _backward_pass_iteration!(optimizer::BendersAlgorithm, i)
 
         # Unfix primal solutions
         PlasmoBenders._unfix_variables(object, var_copies)
+
         _reset_integrality(optimizer, object)
     end
 
